50a83c1edd28c6fb2759784b185097a874dce767
“Suresh.Kumar” <Suresh.Kumar@causeway.com>
2017-08-12 20:18:16 +0530

    .

---
+++ Library/launchgoogle.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      suresh.kumar
+#
+# Created:     12-08-2017
+# Copyright:   (c) suresh.kumar 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+
+browser.get('http://google.co.in')
